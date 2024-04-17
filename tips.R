272c12c6e75b6f83273ee31e957374489e8aa689
Kathleen Morrill <34375512+tabbzi@users.noreply.github.com>
2024-04-17 14:32:53 -0400

    Create tips.R

---
+++ tips.R
@@ -0,0 +1,9 @@
+# Good practices no matter whether using LLMs or not -- but especially handy for LLMs!
+
+# Parse methods text from scientific papers into psuedocode then code
+
+# Write clear pseudocode for what you want to do
+
+# Come up with small toy examples of data that function should work on
+
+# Document your code and learn to write it in roxygen format
